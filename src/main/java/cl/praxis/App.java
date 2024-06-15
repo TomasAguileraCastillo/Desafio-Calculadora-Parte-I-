e5bc25622c9fffeed56580e2120bc189bf761437
Tomás Aguilera <136927556+TomasAguileraCastillo@users.noreply.github.com>
2024-06-14 22:46:08 -0400

    Correccion de texto

--- src/main/java/cl/praxis/App.java
+++ src/main/java/cl/praxis/App.java
@@ -12,8 +12,10 @@ public class App
     {
         Calculadora calculadora = new Calculadora(35,25);
 
-        System.out.println("Obtener el la cuatro poperaciones basicas de los siguientes numeros");
+        System.out.println();
+        System.out.println("El Resultado de las cuatro Operaciones basicas de los siguientes numeros");
         System.out.println("35 y 25");
+        System.out.println();
         System.out.println("La suma es : " +calculadora.sumar());
         System.out.println("La resta es : " +calculadora.restar());
         System.out.println("La Multiplicacion es :  " +calculadora.multiplicar());
